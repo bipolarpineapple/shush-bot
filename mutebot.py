8fc20deec96637a681675ab9ed29bf15812e37bc
FungZhiUwU <86178124+FungZhiUwU@users.noreply.github.com>
2021-07-04 11:48:57 +0800

    mutebot 0.1
    Beta Version 0.1
    - added function to mute and unmute member
    - added new banned word

--- mutebot.py
+++ mutebot.py
@@ -1,11 +1,11 @@
 import discord #for discord bot
 import random #for random function
+from discord.ext import commands
 
-client = discord.Client() #lauch discord client
+client = commands.Bot(command_prefix="+") #lauch discord client
 
-
-nsfwWords = ['porn','sex','faggot','nigga','fuck','shit','simp','furry','sit on me', 'sohai',''] #banned word compilation
-warningWords = ['"Mine" your word','Mind your tongue','FBI warning','Mind your language','"Mine" your language','Oopsie Woopsie, a banned word had occured '] #warning word compilation
+nsfwWords = ['porn','sex','faggot','nigga','fuck','shit','simp','furry','sit on me','jav','nigger'] #banned word compilation
+warningWords = ['"Mine" your word','Mind your tongue','FBI warning','Mind your language','"Mine" your language'] #warning word compilation
 
 @client.event 
 async def on_ready():
@@ -21,7 +21,25 @@ async def on_message(message):
     if any(line in nsfw for line in nsfwWords): #for any word in user message that have said the nsfwWords
         warning = random.choice(warningWords) #the bot will random pick a warningWords
         await message.channel.send(warning) #send the warning
+        await message.delete()
+
+    await client.process_commands(message)
+
+@client.command() #mute members
+@commands.has_permissions(kick_members=True)
+async def mute(ctx, member : discord.Member, *,reason=None):
+    role = ctx.guild.get_role(860927818149658664)
+    await ctx.send(f"Muted because of {reason}")
+    await member.add_roles(role, reason=reason)
+    
+    
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def unmute(ctx, member : discord.Member):
+    role = ctx.guild.get_role(860927818149658664)
+    await ctx.send("has been unmuted")
+    await member.remove_roles(role)
     
 
-TOKEN = ' ' #replace if the TOKEN is regenerated
+TOKEN = 'ODYwODI1NzkxMzg2MzUzNjc1.YOA4fg.9mdIkAD1E21Aw_LtIunYtwAff94' #replace if the TOKEN is regenerated
 client.run(TOKEN)
